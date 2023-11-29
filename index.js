91634d6b68b6b01fbf5a6e9b7475ff1e08221381
sumeetdeshpande1811 <deshpande.su@northeastern.edu>
2023-11-29 16:11:08 -0500

    updating index file

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ exports.handler = async function (event) {
                 from: "no-reply@demo.sumeetdeshpande.me",
                 to:  email,
                 subject: 'Download Failed',
-                text: 'Your file has been failed uploaded to the GCP Bucket'
+                text: 'Your file has been failed uploaded to the GCP Bucket. Issue with Downloading URL'
               };
             await sendEmail(mailgun, emailData);
             return "file not uploaed";
